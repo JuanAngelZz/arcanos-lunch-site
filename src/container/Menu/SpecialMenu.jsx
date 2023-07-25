71f9216a33c0aae156eab688bd858f76d5332cea
Juan Angel <angeluz0226@gmail.com>
2023-07-25 13:29:45 -0400

    correciones

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -10,12 +10,12 @@ const SpecialMenu = () => (
       <h1 className="headtext__alternative">Nuestro Menú</h1>
     </div>
 
-    <div className="app__specialMenu-menu_img">
+    <div className="app__specialMenu-menu_img" data-aos="zoom-out" data-aos-delay="200" data-aos-duration="1000">
         <img src={images.menu} alt="menu" />
     </div>
 
     <div style={{ marginTop: "15px" }}>
-      <a href="#" target="__blank" className="custom__button">
+      <a href="/arcanos-lunch-site/src/assets/menu.jpeg" target="__blank" className="custom__button">
         Ver Completo
       </a>
     </div>
